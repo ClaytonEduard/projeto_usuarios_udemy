91aeda46d7e05db6101a9708675ce3513748095f
claytoneduard <clayton_eduard@hotmail.com>
2022-04-28 12:02:29 -0300

    criando as linhas de inserção do cadastro html

--- .history/index_20220428112625.js
+++ .history/index_20220428112625.js
@@ -8,15 +8,18 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {}
 
 function addLine(dataUser) {
-
+    console.log(dataUser)
     var tr = document.createElement("tr");
-    tr.innerHTML = 
-    '<tr><td> <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"> </td> ' +
-        ' <td>dataUser.name</td> td>fulano@hcode.com.br</td> <td>Sim</td>' + '<td>02/04/2018</td>' +
-         '<td><button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>' +
-        '<button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button></td></tr>'
-
-        ;
+    tr.innerHTML = '
+        < tr >
+    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+    <td>dataUser.name</td><td>fulano@hcode.com.br</td><td>Sim</td><td>02/04/2018</td>
+    <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </ >
+        ';
     document.getElementById("table-users").appendChild(tr)
 }
 
--- .history/index_20220428115033.js
+++ .history/index_20220428115033.js
@@ -10,13 +10,20 @@ var user = {}
 function addLine(dataUser) {
 
     var tr = document.createElement("tr");
-    tr.innerHTML = 
-    '<tr><td> <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"> </td> ' +
-        ' <td>dataUser.name</td> td>fulano@hcode.com.br</td> <td>Sim</td>' + '<td>02/04/2018</td>' +
-         '<td><button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>' +
-        '<button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button></td></tr>'
-
-        ;
+    tr.innerHTML = '
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+             <td>dataUser.name</td>
+             dataUser.
+            <td>fulano@hcode.com.br</td>
+            <td>Sim</td>
+            <td>02/04/2018</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+    </tr >
+        ';
     document.getElementById("table-users").appendChild(tr)
 }
 
